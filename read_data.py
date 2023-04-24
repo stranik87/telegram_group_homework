8161aca6706ac777727085e99f1c4d6dbb03340b
stranik87 <tulkin.boynazarov@gmail.com>
2023-04-24 16:10:12 +0500

    read

--- read_data.py
+++ read_data.py
@@ -9,5 +9,8 @@ def read_data(file_path: str)->dict:
         dict: Dictionary containing the data of the json file.
     
     """
-    #open file
-    return
+    f = open(file_path, encoding="utf8").read()
+    x = json.loads(f)
+    return x
+    
+
